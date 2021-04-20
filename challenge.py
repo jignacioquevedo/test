65d3a279ee1964bd77c607dfadb2c7ab81fdd441
jignacioquevedo <82887314+jignacioquevedo@users.noreply.github.com>
2021-04-20 18:20:27 -0300

    Update challenge.py

--- challenge.py
+++ challenge.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-with open("C:/Users/Chino/Documents/Cursos/Santander Tecnologia/datos_data_engineer.tsv") as tsv_file:
+with open("datos_data_engineer.tsv") as tsv_file:
     data = []
 
     for line in tsv_file:
@@ -11,4 +11,4 @@ new_header = df.iloc[0]
 df = df[1:]
 df.columns = new_header
 
-df.to_csv('C:/Users/Chino/Documents/Cursos/Santander Tecnologia/data_engineer.csv', sep='|', encoding='UTF-8')
+df.to_csv('data_engineer.csv', sep='|', encoding='UTF-8')
